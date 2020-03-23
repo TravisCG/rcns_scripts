a83091ecd605a2d41abce54699f773fba79977c5
TravisCG <buldozer88@hotmail.com>
2020-03-23 07:38:39 +0100

    kmer finder

--- findkmers.py
+++ findkmers.py
@@ -28,5 +28,8 @@ for fastq in allfastq:
 					totalcount += 1
 					foundkmer.add(subseq)
 		count += 1
+		if count % 1000000 == 0:
+			print("Process:", count, file=sys.stderr)
 	infile.close()
+	print("Done:", fastq, file=sys.stderr)
 	print(fastq, len(foundkmer), totalcount, sep = "\t")
---
+++ findonekmer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+import gzip
+
+kmersize = 100
+
+kmer = set()
+kmerfile = open(sys.argv[1])
+for i in kmerfile:
+	kmer.add(i.rstrip())
+kmerfile.close()
+
+fastq = sys.argv[2]
+
+infile = gzip.open(fastq)
+count = 0
+totalcount = 0
+foundkmer = set()
+for i in infile:
+	line = i.decode('utf-8').rstrip()
+	if count % 4 == 1:
+		for startpos in range(0, len(line)-kmersize):
+			subseq = line[startpos:startpos+kmersize]
+			if subseq in kmer:
+				totalcount += 1
+				foundkmer.add(subseq)
+	count += 1
+	if count % 1000000 == 0:
+		print("Process:", count, file=sys.stderr)
+infile.close()
+print(fastq, len(foundkmer), totalcount, sep = "\t")
